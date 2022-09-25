05ba35acaeb019d8a14fe31521abee6889de9bf2
Bryan Renteria <Bryan.Renteria@pulte.com>
2022-09-25 18:27:47 -0500

    control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -15,34 +15,17 @@ export const useShoppingCart = () => {
   }) => {
     console.log("onProductCountChange", count, product);
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        // delete oldShoppingCart[product.id];
+        // return { ...oldShoppingCart };
+        const {[product.id]:toDelete, ...rest} = oldShoppingCart;
+        return rest;
       }
 
-      // borrar el producto
-
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
-
-      // if (count === 0) {
-      //   // delete oldShoppingCart[product.id];
-      //   // return { ...oldShoppingCart };
-      //   const {[product.id]:toDelete, ...rest} = oldShoppingCart;
-      //   return rest;
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: {...product, count  },
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]: {...product, count  },
+      };
     });
   };
 
