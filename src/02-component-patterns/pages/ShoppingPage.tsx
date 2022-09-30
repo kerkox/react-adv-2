bc4b75930ff5328ef6ad44f3f6d65763629e350c
Bryan Renteria <Bryan.Renteria@pulte.com>
2022-09-30 16:36:37 -0500

    Avanzando en el proyecto

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -18,7 +18,6 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
           maxCount: 10,
@@ -26,18 +25,9 @@ export const ShoppingPage = () => {
       >
         {({ reset, count, increaseBy, isMaxCountReached }) => (
           <>
-            <ProductImage
-              className="custom-image"
-              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
-            />
-            <ProductTitle title={"Coffe"} className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}> -2 </button>
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(+2)}> +2 </button>
-            )}
-            <span>count: {count}</span>
+            <ProductImage/>
+            <ProductTitle />
+            <ProductButtons />            
           </>
         )}
       </ProductCard>
